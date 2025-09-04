d9a80a86237c943c37e87a426705b59b68c92685
Kyle Dando <kyle.dando@nxp.com>
2025-09-04 16:12:27 -0500

    removed error and branch restriction

--- my_app/hello_world.c
+++ my_app/hello_world.c
@@ -36,7 +36,7 @@ int main(void)
     /* Init board hardware. */
     BOARD_InitHardware();
 
-    PRINTF("Hello NXP, Eric is a showoff!);
+    PRINTF("Hello NXP, edit here\r\n");
 
     while (1)
     {
